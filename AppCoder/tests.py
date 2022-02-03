1a424c02017a1ba0b36346cfb4ab43e6c847965e
Candelaria Sanchez <sanchezgcandelaria@gmail.com>
2022-02-03 12:53:26 -0300

    completo

--- AppCoder/tests.py
+++ AppCoder/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from AppCoder.models import Profesor
+
+class ProfesorTestCase(TestCase):
+
+    def test_crear_un_profesor(self):
+        profesor = Profesor(nombre='Martín', apellido='Gotelli',
+        email='martingotelli@a.com', profesion='Profesor de Python')
+
+        profesor.save()
+        
+        profesor_en_base = Profesor.objects.get(id=profesor.id)
+
+        self.assertEquals(profesor_en_base.nombre, 'Martín')
+        self.assertEquals(profesor_en_base.apellido, 'Gotelli')
+        self.assertEquals(profesor_en_base.email, 'martingotelli@a.com')
+        self.assertEquals(profesor_en_base.profesion, 'Profesor de Python')
